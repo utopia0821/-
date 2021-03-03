9a7d81243c90b24a191f8d775010909ec141e8a4
utopia0821 <79785599+utopia0821@users.noreply.github.com>
2021-03-03 19:07:55 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from myMath import myStatistics as S
+a = int(input('請輸入第一個數:'))
+b = int(input('請輸入第二個數:'))
+c = int(input('請輸入第三個數:'))
+d = int(input('請輸入第四個數:'))
+e = int(input('請輸入第五個數:'))
+f = [a,b,c,d,e]
+print('平均數為:',S.myMean(f))
